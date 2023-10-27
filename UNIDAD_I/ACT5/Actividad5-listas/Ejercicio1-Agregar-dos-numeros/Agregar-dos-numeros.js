441854c2fb62f786b6d4cf52b7d441644d981cf7
JEstebanSanti <a20311083@uthermosillo.edu.mx>
2023-10-26 18:04:49 -0700

    inicio de listas enlazadas

---
+++ UNIDAD_I/ACT5/Actividad5-listas/Ejercicio1-Agregar-dos-numeros/Agregar-dos-numeros.js
@@ -0,0 +1,14 @@
+class Nodo {
+    constructor(data){
+        this.data = data
+        this.next = null
+    }
+}
+
+class LinkedList {
+    constructor(){
+        this.head = null
+    }
+    // Metodo para Agregar un Nodo al final de la lista
+    
+}
